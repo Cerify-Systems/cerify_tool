4cc5c6b9ce48949d130be406b5f09ee8b7a551a0
Nikhil Gupta <177606878+BitByNIK@users.noreply.github.com>
2025-08-13 18:18:34 +0530

    added env variables for tool frontend also

--- Frontend/src/Routes/Home.jsx
+++ Frontend/src/Routes/Home.jsx
@@ -15,7 +15,7 @@ const contentMap = {
   'Help': Help,
 };
 
-const API_BASE_URL = 'http://127.0.0.1:5000';
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 function Home() {
   const [file, setFile] = useState(null);
